c3e5c4f8b40f6d0b5a7207fc3ceb1ef73eb5313e
MochaChoco <asdswj@gmail.com>
2024-05-16 00:41:47 +0900

    README.md 수정

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
+## Intersection Observer API를 활용한 인피니티 스크롤
+
+![](https://velog.velcdn.com/images/swj9077/post/94415019-226d-46dd-90c7-8e82a0921219/image.gif)
+
+이 프로젝트는 Intersection Observer API를 활용한 인피니티 스크롤 최적화 기법을 구현한 것입니다. 이 프로젝트에서는 Object-pool 패턴을 적용하여 새로운 데이터를 로드할 때마다 새로운 DOM을 생성하는 것이 아니라, 미리 생성된 DOM을 재활용하여 성능을 최적화하는 방법을 구현했습니다.
+
+## 설치 및 실행 방법
+
+### dependency 설치
+
+yarn install
+
+### front-end 실행
+
 yarn dev
 
+### back-end 실행
+
+json-server를 사용하므로 별도의 터미널에서 실행해야 합니다.
+
 yarn json-server --watch db.json --port 3001
+
+## 상세 설명
+
+보다 상세한 설명은 아래의 링크에서 확인하실 수 있습니다.
+
+[Velog 바로가기](https://velog.io/@swj9077/Intersection-Observer-API%EB%A5%BC-%ED%99%9C%EC%9A%A9%ED%95%9C-Infinite-Scroll-%EC%B5%9C%EC%A0%81%ED%99%94%ED%95%98%EA%B8%B0)
--- src/components/InfiniteScroll.vue
+++ src/components/InfiniteScroll.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="root" ref="rootRef">
-    <div class="container" ref="scrollContainerRef">
+    <div class="scrollContainer" ref="scrollContainerRef">
       <div ref="loadMoreTriggerTopRef" class="load-more-trigger-top"></div>
       <div v-for="(item, index) in visibleItems" :key="index" class="item">
         {{
@@ -40,16 +40,18 @@ const rootRef = ref<HTMLElement | null>(null);
 const scrollContainerRef = ref<HTMLElement | null>(null);
 
 const loading = ref(false);
-const page = ref(0);
+const page = ref(0); // mount 되면서 1로 변경됨.
 const pageSize = 20;
-const prevDireciton = ref<"up" | "down">("down");
-const bufferSize = pageSize * 3; // 버퍼 크기를 3페이지 크기로 설정
+const prevDireciton = ref<"up" | "down">("down"); // 이전에 어느 방향에서 데이터를 불러왔는지 저장한다.
+const bufferSize = pageSize * 3; // 화면에 표시할 총 아이템의 갯수. 화면에 보이는 것과 화면 위와 아래에 표시할 것을 고려하여 버퍼 크기를 크기의 3배로 설정한다.
 
+// 스크롤 처리 함수
 const loadItems = async (direction: "up" | "down") => {
   if (loading.value) return;
   loading.value = true;
 
   try {
+    // 이전의 스크롤 방향과 현재 방향을 비교하여 올바른 page 번호를 계산함
     const newPage =
       direction === "down"
         ? prevDireciton.value === "down"
@@ -66,17 +68,19 @@ const loadItems = async (direction: "up" | "down") => {
 
     switch (direction) {
       case "down":
-        // 스크롤 높이 추가
+        // 화면을 내리면 스크롤 높이를 추가하는 로직
         if (page.value >= 1) {
           const item = document.getElementsByClassName("item")[0];
           const rootHeight =
             (page.value + 1) * pageSize * item.getClientRects()[0].height;
           rootRef.value!.style.height = rootHeight + "px";
         }
+
+        // 현재 아이템들과 bufferSize가 일치하는지 체크하여 데이터를 삽입할지 대체할지 분기 처리함.
         if (visibleItems.value.length < bufferSize) {
           visibleItems.value.push(...data.data);
         } else {
-          // 스크롤 위치 조정
+          // 리스트 위치 조정
           const item = document.getElementsByClassName("item")[0];
           scrollContainerRef.value!.style.top = "";
           scrollContainerRef.value!.style.bottom =
@@ -98,7 +102,7 @@ const loadItems = async (direction: "up" | "down") => {
         } else {
           const item = document.getElementsByClassName("item")[0];
 
-          // 스크롤 위치 조정
+          // 리스트 위치 조정
           scrollContainerRef.value!.style.bottom =
             Number(scrollContainerRef.value!.style.bottom.replace("px", "")) +
             pageSize * item.getClientRects()[0].height +
@@ -115,7 +119,7 @@ const loadItems = async (direction: "up" | "down") => {
 
     page.value = newPage;
     totalItems.value = data;
-    prevDireciton.value = direction;
+    prevDireciton.value = direction; // 현재 스크롤 위치를 저장하여 다음 로드때 사용함
   } catch (error) {
     console.error("Failed to load items:", error);
   } finally {
@@ -123,6 +127,7 @@ const loadItems = async (direction: "up" | "down") => {
   }
 };
 
+// 호출된 observer에 따른 분기 처리
 const onIntersect = (
   entries: IntersectionObserverEntry[],
   observer: IntersectionObserver
@@ -136,13 +141,17 @@ const onIntersect = (
   }
 };
 
+// .loadMoreTriggerTop이 화면에 들어왔을 때 (스크롤을 위로 올렸을 때)
 const observerTop = new IntersectionObserver((entries) => {
   if (page.value > 1) onIntersect(entries, observerTop);
 });
+
+// .loadMoreTriggerBottom이 화면에 들어왔을 때 (스크롤을 아래로 내렸을 때)
 const observerBottom = new IntersectionObserver((entries) => {
   if (page.value < totalItems.value.last) onIntersect(entries, observerBottom);
 });
 
+// mount하면 observer를 등록하고, 데이터를 로드한다.
 onMounted(async () => {
   if (loadMoreTriggerTopRef.value) {
     observerTop.observe(loadMoreTriggerTopRef.value);
@@ -153,6 +162,7 @@ onMounted(async () => {
   loadItems("down");
 });
 
+// unmount하면 지정된 observer를 해제하여 불필요한 메모리 낭비를 막는다.
 onUnmounted(() => {
   if (loadMoreTriggerTopRef.value) {
     observerTop.unobserve(loadMoreTriggerTopRef.value);
@@ -164,7 +174,7 @@ onUnmounted(() => {
 </script>
 
 <style>
-.container {
+.scrollContainer {
   position: relative;
   max-width: 600px;
   margin: 0 auto;
